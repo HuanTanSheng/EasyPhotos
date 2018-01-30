d641da8558676a066146040c01d6a896692638f4
桓一文 <35919502@qq.com>
2018-01-30 17:08:54 +0800

    2.2.9

--- sample/src/main/java/com/huantansheng/easyphotos/sample/MainAdapter.java
+++ sample/src/main/java/com/huantansheng/easyphotos/sample/MainAdapter.java
@@ -38,7 +38,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainVH> {
     public void onBindViewHolder(MainVH holder, int position) {
         Photo photo = list.get(position);
         mGlide.load(photo.path).into(holder.ivPhoto);
-        holder.tvMessage.setText("[图片名称]： "+photo.name+"\n[宽]："+photo.width+"\n[高]："+photo.height+"\n[文件大小,单位bytes]："+photo.size+"\n[日期，时间戳，秒]："+photo.time+"\n[图片地址]："+photo.path+"\n[图片类型]："+photo.type+"\n[是否选择原图]："+photo.selectedOriginal);
+        holder.tvMessage.setText("[图片名称]： "+photo.name+"\n[宽]："+photo.width+"\n[高]："+photo.height+"\n[文件大小,单位bytes]："+photo.size+"\n[日期，时间戳，毫秒]："+photo.time+"\n[图片地址]："+photo.path+"\n[图片类型]："+photo.type+"\n[是否选择原图]："+photo.selectedOriginal);
     }
 
     @Override
